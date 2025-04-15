4325ab1a020f7d1dae0f48c8d2bb34fa5e7d2eae
Rahul Balasubramani <rahulb6@illinois.edu>
2025-04-14 22:33:04 -0500

    dist

--- code/main.py
+++ code/main.py
@@ -6,10 +6,12 @@ Author: Rahul Balasubramani(rahulb6) & Anushree Udhayakumar(au11)
 
 import random
 import matplotlib.pyplot as plt
+from geopy.distance import geodesic
 
+# PERT function to generate sample
 def pert_sample(min_val, mode_val, max_val):
     """
-    Generate a sample from a simplified PERT distribution using the mode.
+    Generates a sample using PERT .
 
     :param min_val: float, worst-case value
     :param mode_val: float, most likely value
@@ -21,19 +23,20 @@ def pert_sample(min_val, mode_val, max_val):
     True
     """
     if not (min_val <= mode_val <= max_val):
-        raise ValueError("Invalid PERT parameters: min <= mode <= max must hold.")
-
-    alpha = 4 * (mode_val - min_val) / (max_val - min_val) + 1
-    beta = 4 * (max_val - mode_val) / (max_val - min_val) + 1
+        raise ValueError("Invalid PERT parameters")
+    alpha  = 4 * (mode_val - min_val) / (max_val - min_val) + 1
+    beta   = 4 * (max_val - mode_val) / (max_val - min_val) + 1
     sample = random.betavariate(alpha, beta)
     return min_val + sample * (max_val - min_val)
 
+#Calculate distance between 2 points
+def calculate_distance(coord1, coord2):
+    return geodesic(coord1, coord2).km
+
 
 if __name__ == "__main__":
-
     print("PERT Testttt ")
     min_val, mode_val, max_val = 0.4, 0.8, 1.0
-
     for i in range(10):
         sample = pert_sample(min_val, mode_val, max_val)
         print(f"Sample {i+1}: {sample:.4f}")
